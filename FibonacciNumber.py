1b7d2fe8d29fe9622b6db7870175616796a5c119
Muskan Makan <90406508+muskaan190@users.noreply.github.com>
2022-07-28 14:19:13 +0530

    Code added

---
+++ FibonacciNumber.py
@@ -0,0 +1,11 @@
+# Given an integer n, calculate n-th fibonacci number
+
+
+def fibonacci(n):
+	fib = [0, 1]
+	for i in range(2, n+1):
+		fib.append(fib[i-1] + fib[i-2])
+	return fib[n]
+
+n=int(input())
+print(fibonacci(n))

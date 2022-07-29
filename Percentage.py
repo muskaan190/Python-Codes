b3a4161f6ff2171cd0853245130cdcaff70cb4e2
Muskan Makan <90406508+muskaan190@users.noreply.github.com>
2022-07-29 12:11:42 +0530

    Code Added

---
+++ Percentage.py
@@ -0,0 +1,24 @@
+# The provided code stub will read in a dictionary containing key/value pairs of name:[marks] for a list of students. Print the average of the marks array for the student name provided, showing 2 places after the decimal.
+
+# Sample Input
+# 3
+# Krishna 67 68 69
+# Arjun 70 98 63
+# Malika 52 56 60
+# Malika
+
+# Sample Output
+# 56.00
+
+
+n = int(input())
+    student_marks = {}
+    for _ in range(n):
+        name, *line = input().split()
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input()
+    avg=student_marks[query_name][0]+student_marks[query_name][1]+student_marks[query_name][2]
+    final=avg/3
+    res = "{:.2f}".format(final)
+    print(res)
